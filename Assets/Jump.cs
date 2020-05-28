2385fd6338dd04106e63473eca2783d832e14cec
rkulwicki-laptop <ryan.kulwicki@valpo.edu>
2020-05-28 06:26:40 -0500

    wip jump

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -10,7 +10,11 @@ public class Jump : MonoBehaviour
 
     public int floorBelow;
 
+    public bool jumping;
+
     private BattleManager _battlemanager;
+
+    public float timeToJump, heightOfJump; 
     // Start is called before the first frame update
     void Start()
     {
@@ -23,11 +27,48 @@ public class Jump : MonoBehaviour
         floorBelow = GetOrderInLayerOfFloorBelow(transform.position);
         if (_battlemanager.state == BattleState.INACTIVE) {
 
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) && !jumping)
             {
-                
+                GameObject objectToMove     = this.gameObject;
+                Vector3 end                 = (this.gameObject.transform.position + new Vector3(0, heightOfJump, 0));
+                float seconds               = timeToJump;
+                int startingFloorOrder      = GetOrderInLayerOfFloorBelow(this.gameObject.transform.position);
+
+                StartCoroutine(JumpAction(objectToMove, end, seconds, startingFloorOrder));
             }
 
         }
     }
+
+
+    //move up
+
+    private IEnumerator JumpAction(GameObject objectToMove, Vector3 end, float seconds, int startingFloorOrder) //move over time
+    {
+
+        var distanceToTravel = end;
+
+        jumping = true;
+        float elapsedTime = 0;
+        Vector3 startingPos = objectToMove.transform.position;
+        Vector3 copyOfStartingPos = startingPos;
+        while (elapsedTime < (seconds/2)) //up
+        {
+            objectToMove.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        elapsedTime = 0;
+        Vector3 newStartingPos = objectToMove.transform.position;
+        while (elapsedTime < (seconds)) //down
+        {
+            objectToMove.transform.position = Vector3.Lerp(newStartingPos, copyOfStartingPos, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        jumping = false;
+    }
+
 }
