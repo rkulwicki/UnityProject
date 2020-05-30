b58ad98982184a6552445a9a6aec01743055cc5b
Ryan Kulwicki <ryan.kulwicki@valpo.edu>
2020-05-30 15:59:02 -0500

    add shadow scale change when jumping

--- Assets/Shadow.cs
+++ Assets/Shadow.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class Shadow : MonoBehaviour
 {
     public Vector3 anchorAdjustment;
-    
+
     //shadow relies on Jump.cs, which is in the parent
     private Jump _jump;
 
@@ -27,6 +27,14 @@ public class Shadow : MonoBehaviour
         {
             gameObject.transform.position = _jump.projectedLanding + anchorAdjustment;
         }
+
+        //change size depending on how far away from the actor the shadow is.
+        Vector3 dis = gameObject.transform.parent.transform.position - gameObject.transform.position + anchorAdjustment;
+        float xScale = 1/(Mathf.Abs(dis.y) + 1); //y = 1/(x+1)
+        gameObject.transform.localScale = new Vector3(
+            xScale,
+            gameObject.transform.localScale.y,
+            gameObject.transform.localScale.z);
     }
 
 }
