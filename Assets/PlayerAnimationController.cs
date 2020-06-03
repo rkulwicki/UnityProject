9388248df4e96c1897c932ae3b7ae3d9fb221b80
rkulwicki-laptop <ryan.kulwicki@valpo.edu>
2020-06-03 16:14:37 -0500

    add PlayerAnimationController

--- Assets/PlayerAnimationController.cs
+++ Assets/PlayerAnimationController.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class PlayerAnimationController : MonoBehaviour
 {
 
+    private Animator _animator;
+
+    private SpriteRenderer _spriteRenderer;
     private Jump _jump;
     private PlayerMove _playerMove;
     private PlayerStats _playerStats;
 
+    public int test;
+
     void Start()
     {
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        _animator = gameObject.GetComponent<Animator>();
         _jump = gameObject.GetComponent<Jump>();
         _playerMove = gameObject.GetComponent<PlayerMove>();
     }
@@ -18,42 +25,71 @@ public class PlayerAnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //check for movement
-
-        //check for jump
-
-        //check for hurt
+        //listen for change
+        if (test == 1)
+            IdleFrontLeft();
+        if (test == 2)
+            WalkFrontLeft();
     }
 
     private void IdleFrontLeft()
     {
-
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 0);
     }
 
     private void IdleFrontRight()
     {
-
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 0);
     }
 
     private void IdleBackLeft()
     {
-
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 1);
     }
 
     private void IdleBackRight()
     {
-
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 1);
     }
 
     private void WalkFrontLeft()
     {
-
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 2);
     }
 
     private void WalkFrontRight()
     {
-
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 2);
     }
 
+    private void WalkBackLeft()
+    {
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 3);
+    }
+
+    private void WalkBackRight()
+    {
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 3);
+    }
+
+    private void JumpRight()
+    {
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 4);
+    }
+
+    private void JumpLeft()
+    {
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 4);
+    }
 
 }
