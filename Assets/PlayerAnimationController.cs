585fbff4ef56682be56803b569a6e58ad43396fc
Ryan Kulwicki <ryan.kulwicki@valpo.edu>
2020-06-04 19:00:49 -0500

    add animations - jumping still needs work though

--- Assets/PlayerAnimationController.cs
+++ Assets/PlayerAnimationController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerAnimationController : MonoBehaviour
 {
 
-    private Animator _animator;
+    public float movementBuffer = 0.01f;
 
+    private Rigidbody2D _rigidBody2d;
+    private Animator _animator;
     private SpriteRenderer _spriteRenderer;
     private Jump _jump;
     private PlayerMove _playerMove;
     private PlayerStats _playerStats;
+    private MovementInfo _mi;
 
     public int test;
 
@@ -20,16 +23,48 @@ public class PlayerAnimationController : MonoBehaviour
         _animator = gameObject.GetComponent<Animator>();
         _jump = gameObject.GetComponent<Jump>();
         _playerMove = gameObject.GetComponent<PlayerMove>();
+        _rigidBody2d = gameObject.GetComponent<Rigidbody2D>();
+        _mi = gameObject.GetComponent<MovementInfo>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //listen for change
-        if (test == 1)
-            IdleFrontLeft();
-        if (test == 2)
+        //this is in order of importance.
+
+        //jumping
+        if (_jump.jumping && _mi.rightFacing && !_mi.upFacing)
+            JumpFrontRight();
+        else if(_jump.jumping && !_mi.rightFacing && !_mi.upFacing)
+            JumpFrontLeft();
+        //jumping back
+        else if (_jump.jumping && _mi.rightFacing && _mi.upFacing)
+            JumpBackRight();
+        else if (_jump.jumping && !_mi.rightFacing && _mi.upFacing)
+            JumpBackLeft();
+
+        //walking
+        else if (_mi.isMoving && _mi.rightFacing && !_mi.upFacing)
+            WalkFrontRight();
+        else if (_mi.isMoving && !_mi.rightFacing && !_mi.upFacing)
             WalkFrontLeft();
+        //walking back
+        else if (_mi.isMoving && _mi.rightFacing && _mi.upFacing)
+            WalkBackRight();
+        else if (_mi.isMoving && !_mi.rightFacing && _mi.upFacing)
+            WalkBackLeft();
+
+        //idle
+        else if (!_mi.isMoving && _mi.rightFacing && !_mi.upFacing)
+            IdleFrontRight();
+        else if (!_mi.isMoving && !_mi.rightFacing && !_mi.upFacing)
+            IdleFrontLeft();
+        //idle back
+        else if (!_mi.isMoving && _mi.rightFacing && _mi.upFacing)
+            IdleBackRight();
+        else if (!_mi.isMoving && !_mi.rightFacing && _mi.upFacing)
+            IdleBackLeft();
+
     }
 
     private void IdleFrontLeft()
@@ -80,16 +115,28 @@ public class PlayerAnimationController : MonoBehaviour
         _animator.SetInteger("AnimatorState", 3);
     }
 
-    private void JumpRight()
+    private void JumpFrontRight()
     {
         _spriteRenderer.flipX = true;
         _animator.SetInteger("AnimatorState", 4);
     }
 
-    private void JumpLeft()
+    private void JumpFrontLeft()
     {
         _spriteRenderer.flipX = false;
         _animator.SetInteger("AnimatorState", 4);
+    }
+
+    private void JumpBackRight()
+    {
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 5);
+    }
+
+    private void JumpBackLeft()
+    {
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 5);
     }
 
 }
