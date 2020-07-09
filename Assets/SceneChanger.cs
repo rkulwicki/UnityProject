a3168ae34b2cc94fc2cb5b8d7a93a275a5814168
Ryan Kulwicki <ryan.kulwicki@valpo.edu>
2020-07-08 20:44:16 -0500

    add scene changer AutoMove and FadeToBlack

--- Assets/SceneChanger.cs
+++ Assets/SceneChanger.cs
@@ -1,48 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
-    public Direction directionToMove;
+    public Vector3 endPosition;
+    public float timeToMove;
 
     private GameObject playerToMove;
-
-    private float speedToMove;
-    private float timeToFadeToBlack;
-
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-
-
-    TODO
-
+    private GameObject mainCamera;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Player") {
             playerToMove = col.gameObject;
-            StartCoroutine(MoveOverSeconds(playerToMove, speedToMove, directionToMove));
+            MoveToPosition(playerToMove.transform, endPosition, timeToMove);
+            FadeToBlack();
+        }
+    }
+
+    public void MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+    {
+        StartCoroutine(MoveToPositionCoroutine(transform, position, timeToMove));
+    }
+
+    private IEnumerator MoveToPositionCoroutine(Transform transform, Vector3 position, float timeToMove)
+    {
+        var currentPos = transform.position;
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / timeToMove;
+            transform.position = Vector3.Lerp(currentPos, position, t);
+            yield return null;
         }
     }
 
 
-    private IEnumerator MoveOverSeconds(GameObject objectToMove, Vector3 end, float seconds)
+    public void FadeToBlack()
     {
-        float elapsedTime = 0;
-        Vector3 startingPos = objectToMove.transform.position;
-        while (elapsedTime < seconds)
+        StartCoroutine(FadeToBlackCoroutine(timeToMove));
+    }
+
+    private IEnumerator FadeToBlackCoroutine(float time)
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        var blackCanvasGroup = mainCamera.transform.Find("BlackCanvas").GetComponent<CanvasGroup>();
+
+        while (blackCanvasGroup.alpha < 1)
         {
-            objectToMove.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
-            elapsedTime += Time.deltaTime;
-            yield return new WaitForEndOfFrame();
+            blackCanvasGroup.alpha += Time.deltaTime / time;
+            yield return null;
         }
-        objectToMove.transform.position = end;
+
+        yield return null;
     }
 }
