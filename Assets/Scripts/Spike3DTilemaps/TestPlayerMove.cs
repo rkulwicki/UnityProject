f49f92dcb2c69b833a0a6b4046a795ce19ee448f
Ryan Kulwicki <ryan.kulwicki@valpo.edu>
2020-08-18 12:49:35 -0500

    wip spike - add canMoveBools, add pseudo z adjustment to transform y

--- Assets/Scripts/Spike3DTilemaps/TestPlayerMove.cs
+++ Assets/Scripts/Spike3DTilemaps/TestPlayerMove.cs
@@ -6,31 +6,43 @@ public class TestPlayerMove : MonoBehaviour
     public float speed;
     public Vector3 pseudo3DPosition;
     public string xPositiveKey, xNegativeKey, yPositiveKey, yNegativeKey, zPositiveKey, zNegativeKey;
+    public bool canMoveXPositive, canMoveXNegative, canMoveYPositive, canMoveYNegative, canMoveZPositive, canMoveZNegative;
 
     protected void JustMove()
     {
         MoveGlobalPosition();
-        transform.position = new Vector3(pseudo3DPosition.x, pseudo3DPosition.y, 0);
+        MoveTransformXandY();
+        AdjustTransformYBasedOnPseudo3DZ();
     }
 
     protected void MoveGlobalPosition()
     {
         //x
-        if (Input.GetKey(xPositiveKey))
+        if (Input.GetKey(xPositiveKey) && canMoveXPositive)
             pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(1, 0, 0), Time.deltaTime * speed);
-        else if (Input.GetKey(xNegativeKey))
+        else if (Input.GetKey(xNegativeKey) && canMoveXPositive)
             pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(-1, 0, 0), Time.deltaTime * speed);
 
         //y
-        if (Input.GetKey(yPositiveKey))
+        if (Input.GetKey(yPositiveKey) && canMoveYPositive)
             pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(0, 1, 0), Time.deltaTime * speed);
-        else if (Input.GetKey(yNegativeKey))
+        else if (Input.GetKey(yNegativeKey) && canMoveYNegative)
             pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(0, -1, 0), Time.deltaTime * speed);
 
         //z
-        if (Input.GetKey(zPositiveKey))
+        if (Input.GetKey(zPositiveKey) && canMoveZPositive)
             pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(0, 0, 1), Time.deltaTime * speed);
-        else if (Input.GetKey(zNegativeKey))
+        else if (Input.GetKey(zNegativeKey) && canMoveZNegative)
             pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(0, 0, -1), Time.deltaTime * speed);
+    }
+
+    protected void MoveTransformXandY()
+    {
+        transform.position = new Vector3(pseudo3DPosition.x, pseudo3DPosition.y, 0);
+    }
+
+    protected void AdjustTransformYBasedOnPseudo3DZ()
+    {
+        transform.position += new Vector3(0, pseudo3DPosition.z, 0);
     }
 }
