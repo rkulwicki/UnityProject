d3b642a95596b4f502dceaf6a3aee0a92b1885c7
Ryan Kulwicki <ryan.kulwicki@valpo.edu>
2020-08-17 20:21:06 -0500

    wip Spike - add test player and test tilemaps

---
+++ Assets/Scripts/Spike3DTilemaps/TestPlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayerMove : MonoBehaviour
+{
+    public float speed;
+    public Vector3 pseudo3DPosition;
+    public string xPositiveKey, xNegativeKey, yPositiveKey, yNegativeKey, zPositiveKey, zNegativeKey;
+
+    protected void JustMove()
+    {
+        MoveGlobalPosition();
+        transform.position = new Vector3(pseudo3DPosition.x, pseudo3DPosition.y, 0);
+    }
+
+    protected void MoveGlobalPosition()
+    {
+        //x
+        if (Input.GetKey(xPositiveKey))
+            pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(1, 0, 0), Time.deltaTime * speed);
+        else if (Input.GetKey(xNegativeKey))
+            pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(-1, 0, 0), Time.deltaTime * speed);
+
+        //y
+        if (Input.GetKey(yPositiveKey))
+            pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(0, 1, 0), Time.deltaTime * speed);
+        else if (Input.GetKey(yNegativeKey))
+            pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(0, -1, 0), Time.deltaTime * speed);
+
+        //z
+        if (Input.GetKey(zPositiveKey))
+            pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(0, 0, 1), Time.deltaTime * speed);
+        else if (Input.GetKey(zNegativeKey))
+            pseudo3DPosition = Vector3.MoveTowards(pseudo3DPosition, pseudo3DPosition + new Vector3(0, 0, -1), Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/Spike3DTilemaps/TestTilemapLogic.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+
+public class TestTilemapLogic : MonoBehaviour
+{
+    private GameObject[] tilemapGameObjects; 
+    public Vector3 _pseudo3DPosition;
+
+    void Start()
+    {
+        var list = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+            list.Add(child.gameObject);
+        }
+        tilemapGameObjects = list.ToArray();
+    }
+
+    void Update()
+    {
+        _pseudo3DPosition = GameObject.Find("TestPlayer").GetComponent<TestPlayer>().pseudo3DPosition;
+        ChangeTilemapLayerByPlayerHeight();
+    }
+
+    private void ChangeTilemapLayerByPlayerHeight()
+    {
+        foreach (var tm in tilemapGameObjects)
+        {
+            if (tm.GetComponent<TilemapRenderer>().sortingLayerName == "FloorBelow" && _pseudo3DPosition.z < tm.GetComponent<TilemapRenderer>().sortingOrder)
+                tm.GetComponent<TilemapRenderer>().sortingLayerName = "FloorAbove";
+
+            if (tm.GetComponent<TilemapRenderer>().sortingLayerName == "FloorAbove" && _pseudo3DPosition.z >= tm.GetComponent<TilemapRenderer>().sortingOrder)
+                tm.GetComponent<TilemapRenderer>().sortingLayerName = "FloorBelow";
+        }
+    }
+}
