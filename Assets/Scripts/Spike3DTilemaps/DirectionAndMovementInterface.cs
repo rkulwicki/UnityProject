35002eeb5504cbb2de345e8c0e7aabb886581e4c
Ryan Kulwicki <ryan.kulwicki@valpo.edu>
2020-12-30 15:47:50 -0600

    add player animations (again)

---
+++ Assets/Scripts/Spike3DTilemaps/DirectionAndMovementInterface.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionAndMovementInterface : MonoBehaviour
+{
+    public Vector3 pseudo3DVelocity;
+    public XYDirection direction;
+
+    private Vector3 _lastPosition;
+    private Pseudo3DPlayer _pseudo3DPlayer;
+    private XYDirection _lastDirection;
+    // Use this for initialization
+    void Start()
+    {
+        _pseudo3DPlayer = gameObject.GetComponent<Pseudo3DPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pseudo3DVelocity = (_pseudo3DPlayer.pseudo3DPosition - _lastPosition) / Time.deltaTime;
+        _lastPosition = _pseudo3DPlayer.pseudo3DPosition;
+        _lastDirection = direction;
+
+        if (pseudo3DVelocity.y > 0) //up
+        {
+            if(pseudo3DVelocity.x > 0)
+            {
+                direction = XYDirection.XPosYPos;
+            }
+            else if (pseudo3DVelocity.x < 0)
+            {
+                direction = XYDirection.XNegYPos;
+            }
+            else //x == 0
+            {
+                if(_lastDirection == XYDirection.XPosYNeg || _lastDirection == XYDirection.XPosYPos)
+                {
+                    direction = XYDirection.XPosYPos;
+                }
+                else
+                {
+                    direction = XYDirection.XNegYPos;
+                }
+            }
+        }
+        else if(pseudo3DVelocity.y < 0) //down
+        {
+            if (pseudo3DVelocity.x > 0)
+            {
+                direction = XYDirection.XPosYNeg;
+            }
+            else if (pseudo3DVelocity.x < 0)
+            {
+                direction = XYDirection.XNegYNeg;
+            }
+            else //x == 0
+            {
+                if (_lastDirection == XYDirection.XPosYNeg || _lastDirection == XYDirection.XPosYPos)
+                {
+                    direction = XYDirection.XPosYNeg;
+                }
+                else
+                {
+                    direction = XYDirection.XNegYNeg;
+                }
+            }
+        }
+        else //y == 0
+        {
+            if (pseudo3DVelocity.x > 0)
+            {
+                //should almost always be facing front if just moving right
+                direction = XYDirection.XPosYNeg;
+            }
+            else if (pseudo3DVelocity.x < 0)
+            {
+                direction = XYDirection.XNegYNeg;
+            }
+            else //x == 0
+            {
+                _lastDirection = direction;
+            }
+        }
+
+        //if (pseudo3DVelocity.x > 0 && pseudo3DVelocity.y <= 0.25)
+        //    direction = XYDirection.XPosYNeg;
+        //else if(pseudo3DVelocity.x < 0 && pseudo3DVelocity.y <= 0.25)
+        //    direction = XYDirection.XNegYNeg;
+        //else if (pseudo3DVelocity.x > 0 && pseudo3DVelocity.y >= 0.25)
+        //    direction = XYDirection.XPosYPos;
+        //else if (pseudo3DVelocity.x < 0 && pseudo3DVelocity.y >= 0.25)
+        //    direction = XYDirection.XNegYPos;
+    }
+}
+
+public enum XYDirection { XPosYPos, XPosYNeg, XNegYPos, XNegYNeg}
---
+++ Assets/Scripts/Spike3DTilemaps/PlayerAnimationController.cs
@@ -0,0 +1,169 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+    private DirectionAndMovementInterface _directionAndMovementInterface;
+    private Vector3 _v3;
+    private XYDirection _xyDir;
+    private float _animationBuffer = 0.25f;
+    private float _jumpAnimationBuffer = 0.1f;
+
+    void Start()
+    {
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        _animator = gameObject.GetComponent<Animator>();
+        _directionAndMovementInterface = transform.parent.GetComponent<DirectionAndMovementInterface>();
+    }
+
+    void Update()
+    {
+        _v3 = _directionAndMovementInterface.pseudo3DVelocity;
+        _xyDir = _directionAndMovementInterface.direction;
+
+        if(_xyDir == XYDirection.XPosYPos)
+        {
+            if(Math.Abs(_v3.z) > 0 + _jumpAnimationBuffer)
+            {
+                JumpBackRight();
+            }
+            else if(Math.Abs(_v3.x) > 0 + _animationBuffer | Math.Abs(_v3.y) > 0 + _animationBuffer)
+            {
+                WalkBackRight();
+            }
+            else
+            {
+                IdleBackRight();
+            }
+        }
+        else if(_xyDir == XYDirection.XPosYNeg)
+        {
+            if (Math.Abs(_v3.z) > 0 + _jumpAnimationBuffer)
+            {
+                JumpFrontRight();
+            }
+            else if (Math.Abs(_v3.x) > 0 + _animationBuffer | Math.Abs(_v3.y) > 0 + _animationBuffer)
+            {
+                WalkFrontRight();
+            }
+            else
+            {
+                IdleFrontRight();
+            }
+        }
+        else if (_xyDir == XYDirection.XNegYPos)
+        {
+            if (Math.Abs(_v3.z) > 0 + _jumpAnimationBuffer)
+            {
+                JumpBackLeft();
+            }
+            else if (Math.Abs(_v3.x) > 0 + _animationBuffer | Math.Abs(_v3.y) > 0 + _animationBuffer)
+            {
+                WalkBackLeft();
+            }
+            else
+            {
+                IdleBackLeft();
+            }
+        }
+        else //_xyDir == XYDirection.XNegYNeg
+        {
+            if (Math.Abs(_v3.z) > 0 + _jumpAnimationBuffer)
+            {
+                JumpFrontLeft();
+            }
+            else if (Math.Abs(_v3.x) > 0 + _animationBuffer | Math.Abs(_v3.y) > 0 + _animationBuffer)
+            {
+                WalkFrontLeft();
+            }
+            else
+            {
+                IdleFrontLeft();
+            }
+        }
+
+
+        //if (_v3.x > 0 && _v3.y > 0)
+        //    WalkFrontRight();
+        //else if (_v3.x < 0)
+        //    WalkFrontLeft();
+        //else
+        //    IdleFrontRight();
+    }
+
+    private void IdleFrontLeft()
+    {
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 0);
+    }
+
+    private void IdleFrontRight()
+    {
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 0);
+    }
+
+    private void IdleBackLeft()
+    {
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 1);
+    }
+
+    private void IdleBackRight()
+    {
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 1);
+    }
+
+    private void WalkFrontLeft()
+    {
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 2);
+    }
+
+    private void WalkFrontRight()
+    {
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 2);
+    }
+
+    private void WalkBackLeft()
+    {
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 3);
+    }
+
+    private void WalkBackRight()
+    {
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 3);
+    }
+
+    private void JumpFrontRight()
+    {
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 4);
+    }
+
+    private void JumpFrontLeft()
+    {
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 4);
+    }
+
+    private void JumpBackRight()
+    {
+        _spriteRenderer.flipX = true;
+        _animator.SetInteger("AnimatorState", 5);
+    }
+
+    private void JumpBackLeft()
+    {
+        _spriteRenderer.flipX = false;
+        _animator.SetInteger("AnimatorState", 5);
+    }
+}
