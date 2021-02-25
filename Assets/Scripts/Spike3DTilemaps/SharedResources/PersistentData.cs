cde417777d7fc07e793b2ab1ad40d3a7517af7ab
Ryan Kulwicki <ryan.kulwicki@valpo.edu>
2021-02-25 13:26:16 -0600

    add stats hud singleton

--- Assets/Scripts/Spike3DTilemaps/SharedResources/PersistentData.cs
+++ Assets/Scripts/Spike3DTilemaps/SharedResources/PersistentData.cs
@@ -19,6 +19,8 @@ public class PersistentData : MonoBehaviour
     public int level;
     public int experience;
     public int health;
+    public int maxHealth;
+    public int seeds;
     public int attackPower;
     public int defensePower;
     public int speed;
