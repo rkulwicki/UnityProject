8ad13bab20a8465b6e77b625e9c0b46e80843f6a
Ryan Kulwicki <ryan.kulwicki@valpo.edu>
2021-06-01 14:51:04 -0500

    add tilemap fog (v2)

--- Assets/Scripts/Spike3DTilemaps/Pseudo3DTilemapFog.cs
+++ Assets/Scripts/Spike3DTilemaps/Pseudo3DTilemapFog.cs
@@ -14,8 +14,8 @@ namespace Assets.Scripts.Spike3DTilemaps
     {
         private GameObject[] tilemapGameObjects;
         private GameObject _player;
-        public int previousZ;
-        public int currentZ;
+        public double previousZ;
+        public double currentZ;
         void Start()
         {
             var list = new List<GameObject>();
@@ -33,7 +33,7 @@ namespace Assets.Scripts.Spike3DTilemaps
 
         void Update()
         {
-            currentZ = (int)_player.GetComponent<Pseudo3DPosition>().pseudo3DPosition.z;
+            currentZ = _player.GetComponent<Pseudo3DPosition>().pseudo3DPosition.z;
 
             //if int change in z
             if (currentZ != previousZ) 
@@ -44,53 +44,15 @@ namespace Assets.Scripts.Spike3DTilemaps
                     //ex: 1        = 2                                               - 1
                     var tzDif = tm.GetComponent<TilemapRenderer>().sortingOrder - currentZ;
 
-                    var rgb = 1f;
-                    var a = 1f;
+                    var rgb = 1.0;
+                    var a = 1.0;
 
-                    //algorithm to add fog:
-                    if (tzDif < -9)
-                        rgb = 0;
-                    else if (tzDif == -9)
-                        rgb = 0.1f;
-                    else if (tzDif == -8)
-                        rgb = 0.2f;
-                    else if (tzDif == -7)
-                        rgb = 0.3f;
-                    else if (tzDif == -6)
-                        rgb = 0.4f;
-                    else if (tzDif == -5)
-                        rgb = 0.5f;
-                    else if (tzDif == -4)
-                        rgb = 0.6f;
-                    else if (tzDif == -3)
-                        rgb = 0.7f;
-                    else if (tzDif == -2)
-                        rgb = 0.8f;
-                    else if (tzDif == -1)
-                        rgb = 0.9f;
-                    //tzDiff == 0 stays the same
-                    else if (tzDif == 1)
-                        a = 0.9f;
-                    else if (tzDif == 2)
-                        a = 0.8f;
-                    else if (tzDif == 3)
-                        a = 0.7f;
-                    else if (tzDif == 4)
-                        a = 0.6f;
-                    else if (tzDif == 5)
-                        a = 0.5f;
-                    else if (tzDif == 6)
-                        a = 0.4f;
-                    else if (tzDif == 7)
-                        a = 0.3f;
-                    else if (tzDif == 8)
-                        a = 0.2f;
-                    else if (tzDif == 9)
-                        a = 0.1f;
-                    else if (tzDif > 1)
-                        a = 0;
+                    if (tzDif < 0)
+                        rgb = 1 + (tzDif / 10);
+                    if (tzDif > 0)
+                        a = 1 - (tzDif / 10);
 
-                    var color = new Color(rgb,rgb,rgb,a);
+                    var color = new Color((float)rgb,(float)rgb,(float)rgb,(float)a);
 
                     tm.GetComponent<Tilemap>().color = color;
                 } 
